365e7ae3f23ffbad678aceb00f3fab2ba2344e2b
Blake Balbirnie <blakebalbirnie@gmail.com>
2018-03-12 18:31:36 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,13 +21,18 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-
+      @user = User.find_by_id(session[:user_id])
+      if @user != nil
+      erb :account
+    else
+      erb :error
+    end
   end
 
   get '/logout' do
-
+    session.clear
+    redirect '/'
   end
 
 
 end
-
